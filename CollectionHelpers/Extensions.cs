17a8d22ecd5fe7a2ccb90521586163ca6517158c
cheeeeeeeeeen <blancfaye7@gmail.com>
2020-11-28 05:55:00 +0800

    Add ConditionalRemove helper

--- CollectionHelpers/Extensions.cs
+++ CollectionHelpers/Extensions.cs
@@ -25,6 +25,21 @@ namespace Chen.Helpers.CollectionHelpers
         }
 
         /// <summary>
+        /// Removes the item from the list if it exists.
+        /// This is just a combination for Contains and Remove.
+        /// </summary>
+        /// <typeparam name="T">Type of the items in the list</typeparam>
+        /// <param name="list">The list in question</param>
+        /// <param name="value">The value to remove from the list</param>
+        /// <returns>True if the item is removed. False if the item does not exist.</returns>
+        public static bool ConditionalRemove<T>(this List<T> list, T value)
+        {
+            if (!list.Contains(value)) return false;
+            list.Remove(value);
+            return true;
+        }
+
+        /// <summary>
         /// Checks if a value exists within the array.
         /// </summary>
         /// <typeparam name="T">Data type of the array</typeparam>
