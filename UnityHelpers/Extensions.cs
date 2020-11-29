7fb6aad5d7c0b13ca29225bbe36e765bdff8f0c7
cheeeeeeeeeen <blancfaye7@gmail.com>
2020-11-30 02:24:09 +0800

    Add extensions to allow components to call GetOrAddComponent

--- UnityHelpers/Extensions.cs
+++ UnityHelpers/Extensions.cs
@@ -24,13 +24,26 @@ namespace Chen.Helpers.UnityHelpers
         }
 
         /// <summary>
+        /// An extension that allows fetching or adding a component through the component of the GameObject.
+        /// If the GameObject has the component, it will only fetch it and return the component.
+        /// If the GameObject does not have the component, it will add one to it and return the component.
+        /// </summary>
+        /// <typeparam name="T">A Unity Component</typeparam>
+        /// <param name="component">The GameObject of the component to fetch from or add to</param>
+        /// <returns>The component specified as T</returns>
+        public static T GetOfAddComponent<T>(this Component component) where T : Component
+        {
+            return component.gameObject.GetOrAddComponent<T>();
+        }
+
+        /// <summary>
         /// An extension that allows fetching or adding a component.
         /// This overload contains post-processing logic of the component.
         /// Only used for cleaner code. Otherwise it is equivalent to assigning values after this method call.
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="gameObject"></param>
-        /// <param name="action"></param>
+        /// <typeparam name="T">A Unity Component</typeparam>
+        /// <param name="gameObject">The GameObject to fetch from or add to</param>
+        /// <param name="action">Post-Processor code</param>
         /// <returns>The component specified as T</returns>
         public static T GetOrAddComponent<T>(this GameObject gameObject, Action<T> action) where T : Component
         {
@@ -40,14 +53,28 @@ namespace Chen.Helpers.UnityHelpers
         }
 
         /// <summary>
+        /// An extension that allows fetching or adding a component through the component of the GameObject.
+        /// This overload contains post-processing logic of the component.
+        /// Only used for cleaner code. Otherwise it is equivalent to assigning values after this method call.
+        /// </summary>
+        /// <typeparam name="T">A Unity Component</typeparam>
+        /// <param name="component">The GameObject of the component to fetch from or add to</param>
+        /// <param name="action">Post-Processor code</param>
+        /// <returns>The component specified as T</returns>
+        public static T GetOfAddComponent<T>(this Component component, Action<T> action) where T : Component
+        {
+            return component.gameObject.GetOrAddComponent(action);
+        }
+
+        /// <summary>
         /// An extension that allows fetching or adding a component.
         /// This overload contains post-processing logic of the component depending whether it was created or fetched.
         /// Only used for advanced logic without having to do an if-else statement after the method call.
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="gameObject"></param>
-        /// <param name="getAction"></param>
-        /// <param name="addAction"></param>
+        /// <typeparam name="T">A Unity Component</typeparam>
+        /// <param name="gameObject">The GameObject to fetch from or add to</param>
+        /// <param name="getAction">Post-Processor code for fetching</param>
+        /// <param name="addAction">Post-Processor code for adding</param>
         /// <returns>The component specified as T</returns>
         public static T GetOrAddComponent<T>(this GameObject gameObject, Action<T> getAction, Action<T> addAction) where T : Component
         {
@@ -59,6 +86,21 @@ namespace Chen.Helpers.UnityHelpers
                 addAction(component);
             }
             return component;
+        }
+
+        /// <summary>
+        /// An extension that allows fetching or adding a component through the component of the GameObject.
+        /// This overload contains post-processing logic of the component depending whether it was created or fetched.
+        /// Only used for advanced logic without having to do an if-else statement after the method call.
+        /// </summary>
+        /// <typeparam name="T">A Unity Component</typeparam>
+        /// <param name="component">The GameObject of the component to fetch from or add to</param>
+        /// <param name="getAction">Post-Processor code for fetching</param>
+        /// <param name="addAction">Post-Processor code for adding</param>
+        /// <returns>The component specified as T</returns>
+        public static T GetOfAddComponent<T>(this Component component, Action<T> getAction, Action<T> addAction) where T : Component
+        {
+            return component.gameObject.GetOrAddComponent(getAction, addAction);
         }
     }
 }
