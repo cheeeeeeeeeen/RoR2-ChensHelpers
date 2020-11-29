8dfc9f52816b260a7b41334e28f2fce73603d8ee
cheeeeeeeeeen <blancfaye7@gmail.com>
2020-11-30 02:06:34 +0800

    Implement Log extensions for collection data

---
+++ LogHelpers/Collections.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+namespace Chen.Helpers.LogHelpers
+{
+    /// <summary>
+    /// Extensions for Log to allow logging with collections of data with ease.
+    /// </summary>
+    public static class Collections
+    {
+        private const string defaultToken = "%DATA%";
+
+        /// <summary>
+        /// Logs an array of values using the Message format. Use the format parameter to control how the log entry will be displayed. Leave as is for plain logs.
+        /// The representation parameter will be the token that will be used to display the data that is specified in the format parameter (default is %DATA%).
+        /// e.g. MessageArray(new string[] { "Chen", "is", "AWESOME" }, "-> %X%..!", "%X%") will yield:
+        /// -> Chen..!
+        /// -> is..!
+        /// -> AWESOME..!
+        /// </summary>
+        /// <typeparam name="T">Type of data in the array</typeparam>
+        /// <param name="logger">Log instance</param>
+        /// <param name="data">The array</param>
+        /// <param name="format">The format that will be outputted per item, with the item represented as %DATA%
+        /// (can be overridden through representation parameter)</param>
+        /// <param name="representation">Token that will be used as the item when parsing the format</param>
+        public static void MessageArray<T>(this Log logger, T[] data, string format = defaultToken, string representation = defaultToken)
+        {
+            GenericLogging(data, format, representation, item => logger.Message(item));
+        }
+
+        /// <summary>
+        /// Logs an array of values using the Info format. Use the format parameter to control how the log entry will be displayed. Leave as is for plain logs.
+        /// The representation parameter will be the token that will be used to display the data that is specified in the format parameter (default is %DATA%).
+        /// e.g. InfoArray(new string[] { "Chen", "is", "AWESOME" }, "-> %X%..!", "%X%") will yield:
+        /// -> Chen..!
+        /// -> is..!
+        /// -> AWESOME..!
+        /// </summary>
+        /// <typeparam name="T">Type of data in the array</typeparam>
+        /// <param name="logger">Log instance</param>
+        /// <param name="data">The array</param>
+        /// <param name="format">The format that will be outputted per item, with the item represented as %DATA%
+        /// (can be overridden through representation parameter)</param>
+        /// <param name="representation">Token that will be used as the item when parsing the format</param>
+        public static void InfoArray<T>(this Log logger, T[] data, string format = defaultToken, string representation = defaultToken)
+        {
+            GenericLogging(data, format, representation, item => logger.Info(item));
+        }
+
+        /// <summary>
+        /// Logs an array of values using the Warning format. Use the format parameter to control how the log entry will be displayed. Leave as is for plain logs.
+        /// The representation parameter will be the token that will be used to display the data that is specified in the format parameter (default is %DATA%).
+        /// e.g. WarningArray(new string[] { "Chen", "is", "AWESOME" }, "-> %X%..!", "%X%") will yield:
+        /// -> Chen..!
+        /// -> is..!
+        /// -> AWESOME..!
+        /// </summary>
+        /// <typeparam name="T">Type of data in the array</typeparam>
+        /// <param name="logger">Log instance</param>
+        /// <param name="data">The array</param>
+        /// <param name="format">The format that will be outputted per item, with the item represented as %DATA%
+        /// (can be overridden through representation parameter)</param>
+        /// <param name="representation">Token that will be used as the item when parsing the format</param>
+        public static void WarningArray<T>(this Log logger, T[] data, string format = defaultToken, string representation = defaultToken)
+        {
+            GenericLogging(data, format, representation, item => logger.Warning(item));
+        }
+
+        /// <summary>
+        /// Logs an array of values using the Error format. Use the format parameter to control how the log entry will be displayed. Leave as is for plain logs.
+        /// The representation parameter will be the token that will be used to display the data that is specified in the format parameter (default is %DATA%).
+        /// e.g. ErrorArray(new string[] { "Chen", "is", "AWESOME" }, "-> %X%..!", "%X%") will yield:
+        /// -> Chen..!
+        /// -> is..!
+        /// -> AWESOME..!
+        /// </summary>
+        /// <typeparam name="T">Type of data in the array</typeparam>
+        /// <param name="logger">Log instance</param>
+        /// <param name="data">The array</param>
+        /// <param name="format">The format that will be outputted per item, with the item represented as %DATA%
+        /// (can be overridden through representation parameter)</param>
+        /// <param name="representation">Token that will be used as the item when parsing the format</param>
+        public static void ErrorArray<T>(this Log logger, T[] data, string format = defaultToken, string representation = defaultToken)
+        {
+            GenericLogging(data, format, representation, item => logger.Error(item));
+        }
+
+        /// <summary>
+        /// Logs an array of values using the Debug format. Use the format parameter to control how the log entry will be displayed. Leave as is for plain logs.
+        /// The representation parameter will be the token that will be used to display the data that is specified in the format parameter (default is %DATA%).
+        /// e.g. DebugArray(new string[] { "Chen", "is", "AWESOME" }, "-> %X%..!", "%X%") will yield:
+        /// -> Chen..!
+        /// -> is..!
+        /// -> AWESOME..!
+        /// </summary>
+        /// <typeparam name="T">Type of data in the array</typeparam>
+        /// <param name="logger">Log instance</param>
+        /// <param name="data">The array</param>
+        /// <param name="format">The format that will be outputted per item, with the item represented as %DATA%
+        /// (can be overridden through representation parameter)</param>
+        /// <param name="representation">Token that will be used as the item when parsing the format</param>
+        public static void DebugArray<T>(this Log logger, T[] data, string format = defaultToken, string representation = defaultToken)
+        {
+            GenericLogging(data, format, representation, item => logger.Debug(item));
+        }
+
+        private static void GenericLogging<T>(T[] data, string format, string token, Action<string> logAction)
+        {
+            foreach (T datum in data)
+            {
+                string entry = "";
+                string[] tokenized = format.Split(new string[] { token }, StringSplitOptions.None);
+                int length = tokenized.Length;
+                for (int i = 0; i < length; i++)
+                {
+                    entry += tokenized[i];
+                    if (i != length - 1) entry += datum.ToString();
+                }
+                logAction(entry);
+            }
+        }
+    }
+}
