92d1a2014a6262d00d7e417f2191e5992de0ef02
cheeeeeeeeeen <blancfaye7@gmail.com>
2020-12-06 08:23:26 +0800

    Implement Component processors

--- UnityHelpers/ListProcessor.cs
+++ UnityHelpers/ListProcessor.cs
@@ -4,34 +4,36 @@ using UnityEngine;
 namespace Chen.Helpers.UnityHelpers
 {
     /// <summary>
-    /// A generic component for implementing a Queue Processor.
-    /// The Queue Processor attempts to process N items per frame, allowing items to be processed over time and not instantaneously.
+    /// A generic component for implementing a List Processor.
+    /// The List Processor will attempt to process all the items in the data structure in a single frame.
     /// </summary>
-    public abstract class QueueProcessor<T> : MonoBehaviour
+    public abstract class ListProcessor<T> : MonoBehaviour
     {
         /// <summary>
-        /// The data structure where all items are queued in for processing.
+        /// The data structure where all items are stored for processing.
         /// </summary>
-        protected Queue<T> processQueue = new Queue<T>();
+        protected List<T> processList = new List<T>();
 
         /// <summary>
-        /// Number of items to process per frame.
+        /// Determines whether the data structure should save failed items back and repeat the process in the same frame.
         /// </summary>
-        protected abstract int itemsPerFrame { get; set; }
+        protected abstract bool repeatUntilSuccess { get; set; }
+
+        private List<T> failedItems = new List<T>();
 
         /// <summary>
         /// A method the contains the main processing logic. It is required to be implemented by the child class.
-        /// This method returns a boolean that determines the outcome of the item from the queue.
+        /// This method returns a boolean that determines the outcome of the item from the list.
         /// Returning true will dispose of the item. Returning false will put the item back at the end of the queue.
         /// </summary>
-        /// <param name="item">Item that was dequeued from the data structure</param>
+        /// <param name="item">Item that was dequeued from the list</param>
         /// <returns>Return true to send a signal of a successful processing, false otherwise.</returns>
         protected abstract bool Process(T item);
 
         /// <summary>
         /// A post-processing method to perform when the operation process is successful. Always invoke base.OnSuccess.
         /// </summary>
-        /// <param name="item">Item that was dequeued from the data structure</param>
+        /// <param name="item">Item that was dequeued from the list</param>
         protected virtual void OnSuccess(T item)
         {
         }
@@ -39,9 +41,10 @@ namespace Chen.Helpers.UnityHelpers
         /// <summary>
         /// A post-processing method to perform when the operation process failed. Always invoke base.OnFailure.
         /// </summary>
-        /// <param name="item">Item that was dequeued from the data structure</param>
+        /// <param name="item">Item that was dequeued from the list</param>
         protected virtual void OnFailure(T item)
         {
+            if (repeatUntilSuccess) failedItems.Add(item);
         }
 
         /// <summary>
@@ -50,18 +53,18 @@ namespace Chen.Helpers.UnityHelpers
         /// </summary>
         protected virtual void FixedUpdate()
         {
-            for (int i = 0; i < itemsPerFrame; i++)
+            while (processList.Count > 0)
             {
-                if (processQueue.Count > 0)
+                foreach (T item in processList)
                 {
-                    T item = processQueue.Dequeue();
-                    bool outcome = Process(item);
-                    if (outcome) OnSuccess(item);
-                    else
-                    {
-                        OnFailure(item);
-                        processQueue.Enqueue(item);
-                    }
+                    bool result = Process(item);
+                    if (result) OnSuccess(item);
+                    else OnFailure(item);
+                }
+                if (repeatUntilSuccess)
+                {
+                    processList.Clear();
+                    processList.AddRange(failedItems);
                 }
                 else break;
             }
