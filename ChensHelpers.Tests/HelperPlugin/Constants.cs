7395ea37a97550ab813896e57ebb07d7ac62e6ca
cheeeeeeeeeen <blancfaye7@gmail.com>
2020-12-08 08:02:11 +0800

    Migrate file structuring to a proper one

---
+++ ChensHelpers.Tests/HelperPlugin/Constants.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Chen.Helpers.Tests.HelperPlugin
+{
+    [TestClass]
+    public class Constants
+    {
+        [TestMethod]
+        public void ModVer_Length_ReturnsCorrectFormat()
+        {
+            string result = Helpers.HelperPlugin.ModVer;
+            const int ModVersionCount = 3;
+
+            int count = result.Split('.').Length;
+
+            Assert.AreEqual(ModVersionCount, count);
+        }
+
+        [TestMethod]
+        public void ModName_Value_ReturnsCorrectName()
+        {
+            string result = Helpers.HelperPlugin.ModName;
+            const string ModName = "ChensHelpers";
+
+            Assert.AreEqual(ModName, result);
+        }
+
+        [TestMethod]
+        public void ModGuid_Value_ReturnsCorrectGuid()
+        {
+            string result = Helpers.HelperPlugin.ModGuid;
+            const string ModGuid = "com.Chen.ChensHelpers";
+
+            Assert.AreEqual(ModGuid, result);
+        }
+    }
+}
---
+++ ChensHelpers.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ChensHelpers.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ChensHelpers.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7890a6e0-cc98-422d-aa8b-b83b304ec046")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
